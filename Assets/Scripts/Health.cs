92e51bf3116a9a57cea7fc14f3cf25c38529e099
dariodaguanno <tul11082@temple.edu>
2024-04-10 13:00:04 -0400

    Health and damage applied
    Asteroids and bullets apply damage.
    Asteroids, player and planet take damage.
    Game ends when player or planet have 0 health.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,59 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Handles GameObject's health
+public class Health : MonoBehaviour {
+
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth = 100;
+
+    // Event invoked when the GameObject receives damage, providing the updated health value
+    [SerializeField] private UnityEvent<int> OnReceiveDamage;
+
+    // Event invoked when the GameObject's health reaches zero
+    [SerializeField] private UnityEvent OnZeroHealth;
+
+    // Event invoked when the GameObject gains health, providing the updated health value
+    [SerializeField] private UnityEvent<int> OnReceiveHealth;
+
+    private void Start() {
+        currentHealth = maxHealth;  
+    }
+
+    public int CurrentHealth {
+        get => currentHealth;  
+        set => currentHealth = value;
+    }
+
+    public int MaxHealth {
+        get => maxHealth;
+        set => maxHealth = value;
+    }
+
+    // Method to apply damage to the GameObject's health
+    public void ReceiveDamage(int damageAmount) {
+
+        CurrentHealth -= damageAmount;
+
+        // Invoke the OnReceiveDamage event, passing the updated health value
+        OnReceiveDamage?.Invoke(CurrentHealth);
+
+        if (CurrentHealth <= 0) {
+            // If health is zero or below, invoke the OnZeroHealth event
+            OnZeroHealth?.Invoke();
+        }
+    }
+
+    // Method to increase the GameObject's health
+    public void GainHealth(int gainAmount) {
+
+        currentHealth += gainAmount;
+
+        // Ensure the current health does not exceed the maximum health
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        // Invoke the OnReceiveHealth event, passing the updated health value
+        OnReceiveHealth?.Invoke(currentHealth);
+    }
+}
